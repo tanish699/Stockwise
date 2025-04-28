44c975e1b15c9587a4dfaf0984fd04778b260d8b
mehak11082005 <mehaksingla1108@gmail.com>
2025-04-28 12:00:11 +0530

    userLogin file

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        
+        if(userLogin.login()){
+            System.out.println("Login Sucessfull");
+        }
+    }
+}
---
+++ userLogin.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class userLogin {
+
+    public static boolean  login() {
+        
+        HashMap<String, String> userDatabase = new HashMap<>();
+
+        
+        userDatabase.put("Isha", "Ishaisha05");
+
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String enteredUsername = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String enteredPassword = sc.nextLine();
+        sc.close();
+
+        
+        if (userDatabase.containsKey(enteredUsername)) {
+            
+            String correctPassword = userDatabase.get(enteredUsername);
+
+            if (correctPassword.equals(enteredPassword)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            
+            System.out.println("Username not found. Please register.");
+        }
+        return false;
+
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Code executed without errors");
+    }
+    
+}
